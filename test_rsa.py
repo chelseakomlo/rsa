4ec291dfed2558241236378d76fb3569a76b5be1
Chelsea H. Komlo <ckomlo@thoughtworks.com>
2015-09-27 15:27:58 -0600

    small fix for test data

--- test_rsa.py
+++ test_rsa.py
@@ -26,22 +26,27 @@ class TestRSA():
     assert_false(result)
 
   def test_generate_pub_key_exponent(self):
-    # should be greater than 1 but less than totient
-    # should be coprime to the totient (share no other factors other than 1)
     n = 7
-    expected_pub_key_exponent = [1, 2]
+    expected_pub_key_exponent = [1, 2, 3]
 
     actual = gen_pub_key_exponent(n)
     assert_true(actual in expected_pub_key_exponent)
 
     m = 11
-    expected_pub_key_exponent = [1, 2, 5]
+    expected_pub_key_exponent = [1, 2, 3, 5]
 
     actual = gen_pub_key_exponent(m)
     assert_true(actual in expected_pub_key_exponent)
 
-  def test_generate_priv_key_exponent(self):
-    pass
+  def generate_priv_key_exponent(self):
+    # (d * e) % φ(n) = 1
 
+    n = 7
+    e = 2
+    actual = gen_priv_exponent(n, e)
+    totient = calc_totient(n)
+    priv_key_exponent_result = (actual *e) % totient
+
+    assert_equals(priv_key_exponent_result, 1) 
 
 
