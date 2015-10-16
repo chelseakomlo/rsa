41ce56559d7b4a50c05c657be3da1b0df7306b95
Chelsea Komlo <ckomlo@nackomlo.local>
2015-10-16 16:44:20 -0400

    working

--- rsa.py
+++ rsa.py
@@ -3,20 +3,13 @@ import random
 def generate_keys(p, q):
   n = calc_modulus(p, q)
   totient = calc_totient(p, q)
-  e = gen_pub_key_exponent(totient)
+  e = 17 
   d = gen_priv_key_exponent(n, totient, e)
 
   return {"modulus": n, "public": e, "private": d}
 
 def calc_modulus(p, q):
   return p*q
-
-def gen_pub_key_exponent(totient):
-  rand_index = random.randrange(1, totient)
-
-  while not is_prime(rand_index):
-    rand_index -= 1
-  return rand_index
 
 def gen_priv_key_exponent(n, totient, e):
   d = 1
@@ -29,14 +22,6 @@ def calc_mod(a, b):
 
 def calc_totient(p, q):
   return (p-1)*(q-1)
-
-def is_prime(n, index=None):
-  if not index: index = n//2
-
-  if index == 1 or index == 0: return True
-  if n % index == 0: return False
-
-  return is_prime(n, index-1)
 
 def encrypt(message, e, n):
   m = to_ascii(list(message))
--- test_rsa.py
+++ test_rsa.py
@@ -5,9 +5,7 @@ from rsa import *
 class TestRSA():
   def build(self):
     p = 53
-    # this q is too large
-    #q = 227
-    q = 61
+    q = 3371
     return generate_keys(p, q)
 
   def test_creation_modulus_n(self):
@@ -23,17 +21,6 @@ class TestRSA():
     totient = calc_totient(p, q)
     assert_equals(20, totient)
 
-  def test_generate_pub_key_exponent(self):
-    expected_pub_key_exponent = [1]
-
-    actual = gen_pub_key_exponent(2)
-    assert_true(actual in expected_pub_key_exponent)
-
-    expected_pub_key_exponent = [1, 2, 3]
-
-    actual = gen_pub_key_exponent(5)
-    assert_true(actual in expected_pub_key_exponent)
-
   def test_generate_priv_key_exponent(self):
     n = 33
     totient = 20
@@ -48,23 +35,23 @@ class TestRSA():
     q = 3
 
     expected_modulus = 6
-    expected_public = 1
+    expected_public = 17
     expected_private = 1
     actual_keys = generate_keys(p, q)
 
     assert_equals(actual_keys["public"], expected_public)
     assert_equals(actual_keys["private"], expected_private)
 
-  def t_encrypt_message(self):
+  def test_encrypt_message(self):
     keys = self.build()
 
     m = "a"
     ciphertext = encrypt(m, keys["public"], keys["modulus"])
-    assert_equals(1632, ciphertext)
+    assert_equals(52300, ciphertext)
 
     m = "aa"
     ciphertext = encrypt(m, keys["public"], keys["modulus"])
-    assert_equals(2894, ciphertext)
+    assert_equals(29606, ciphertext)
 
   def test_crypt(self):
     m = 9
@@ -74,16 +61,16 @@ class TestRSA():
     ciphertext = crypt(m, e, n)
     assert_equals(1972, ciphertext)
 
-  def t_decrypt(self):
+  def test_decrypt(self):
     keys = self.build()
 
-    ciphertext = 1632
+    ciphertext = 52300
     message = decrypt(ciphertext, 1, keys["private"], keys["modulus"])
     assert_equals("a", message)
 
-    ciphertext_2 = 2069
+    ciphertext_2 = 29606
     message = decrypt(ciphertext_2, 2, keys["private"], keys["modulus"])
-    assert_equals("ab", message)
+    assert_equals("aa", message)
 
   def test_to_ascii(self):
     message_one = ["a"]
@@ -99,7 +86,7 @@ class TestRSA():
     assert_equals("a", from_ascii(97, 1))
     assert_equals("ab", from_ascii(24930, 2))
 
-  def t_sanity__integration(self):
+  def test_sanity__integration(self):
     keys = self.build()
 
     message = "aa"
